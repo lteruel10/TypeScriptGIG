ca8820c9cd8bab3b0dbcc51cf4288d0ce8f0f588
lteruel10 <lteruel10@gmail.com>
2021-07-09 16:00:44 -0600

    19. Desestructruación de Objetos y Arreglos

--- app.js
+++ app.js
@@ -1,21 +1,68 @@
 "use strict";
 (function () {
-    "use strict";
-    (function () {
-        //function activar(quien:string, objeto:string){ objeto es obligatorio
-        //los objetos deben ir agregados de la siguietne manera
-        //1 obligatorios, 2 ?opcionales y3 valores por defecto
-        function activar(quien, momento, objeto) {
-            if (objeto === void 0) { objeto = 'batiseñal'; }
-            if (momento) {
-                console.log(quien + " activ\u00F3 la " + objeto + " en la " + momento);
-            }
-            else {
-                console.log(quien + " activ\u00F3 la " + objeto);
-            }
-        }
-        activar('Gordon', 'brithney señal', 'en la tarde');
-    })(); //funcion autoinvocada   
-    ///let apunta a un espacio en memoria.
-})(); //funcion autoinvocada   
-///let apunta a un espacio en memoria.
+    var AVENGER = {
+        nombre: "Steve",
+        clave: "Capitan America",
+        poder: 'Droga'
+    };
+    console.log(AVENGER.nombre);
+    console.log(AVENGER.clave);
+    console.log(AVENGER.poder);
+    ///deserstruturacion de objetos
+    var nombre = AVENGER.nombre, clave = AVENGER.clave, poder = AVENGER.poder;
+    console.log(nombre);
+    console.log(clave);
+    console.log(poder);
+    //detnro de una funcion
+    var extraer = function (AVENGER) {
+        var nombre = AVENGER.nombre, clave = AVENGER.clave, poder = AVENGER.poder;
+        console.log('\n');
+        console.log(nombre);
+        console.log(clave);
+        console.log(poder);
+    };
+    extraer(AVENGER);
+    //detnro de una funcion
+    var extraer2 = function (_a) {
+        var nombe = _a.nombe, poder = _a.poder, clave = _a.clave;
+        ////////woooooooow super interesante///[]son corchetes.... {}son llaves para otros objetos
+        console.log('\n');
+        console.log(nombre);
+        console.log(poder);
+    };
+    extraer2(AVENGER);
+    ///desestructuracion de arreglos
+    var avengerA = ['Thor', 'ironMan', 'Spiderman'];
+    console.log('\n');
+    console.log(avengerA[0]);
+    console.log(avengerA[1]);
+    console.log(avengerA[2]);
+    ///desestructurar arreglo
+    //const[thor,ironman,spiderman] =avengerA;///[]son corchetes.... {}son llaves para otros objetos
+    /*const[t,i,s] =avengerA;///[]son corchetes.... {}son llaves para otros objetos
+    console.log('\n')
+    console.log(t);
+    console.log(i);
+    console.log(s);*/
+    //si solo interesa un elemento
+    var s = avengerA[2]; //solo se agregan las comas de los elementos que se van a obviar
+    console.log('\n');
+    console.log(s);
+    var extraerArr = function (avengerA) {
+        console.log('\n desestructurar arreglo en flecha');
+        console.log(avengerA[0]);
+        console.log(avengerA[1]);
+        console.log(avengerA[2]);
+    };
+    extraerArr(avengerA);
+    var extraerA2 = function (_a) {
+        var thor = _a[0], ironman = _a[1], spiderman = _a[2];
+        console.log('\n desestructurar arreglo en FLECHA');
+        console.log(thor);
+        console.log(ironman);
+        console.log(spiderman);
+    };
+    extraerA2(avengerA);
+    // setTimeout(function() {location.reload()},5000// Interval time
+    // );
+})(); //FUNCION AUTOINVOCADA CON FLECHA
