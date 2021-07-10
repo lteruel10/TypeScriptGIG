5ff762a59fe12eb9f5f098dbd46a3bf8cba3847b
lteruel10 <lteruel10@gmail.com>
2021-07-10 10:21:51 -0600

    25. Constructores de una clase en TypeScript

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 "use strict";
 (() => {
+    /* ///CLASES
+   class Avenger{
+     //nombre:string='sin  nombre';// se deben inicializar en el constructor
+     nombre:string;
+     equipo:string;
+     nombreReal:string;
+     puedePelear:boolean;
+     peleasGanadas:number;
+   //constructor
+     constructor(nombre:string,equipo:string){
+       this.nombre=nombre;
+       this.equipo=equipo;
+       this.nombre=nombre;
+       this.nombre=nombre;
+     }
+   }//Avenger*/
     ///CLASES 
     class Avenger {
-        constructor() {
-            this.nombre = 'sin  nombre'; // se deben inicializar en el constructor
+        //nombre:string='sin  nombre';// se deben inicializar en el constructor
+        /*   nombre:string;
+           equipo:string;
+           nombreReal:string;
+           puedePelear:boolean;
+           peleasGanadas:number;*/
+        //constructor
+        constructor(nombre, equipo, nombreReal, puedePelear, peleasGanadas = 0) {
+            this.nombre = nombre;
+            this.equipo = equipo;
+            this.nombreReal = nombreReal;
+            this.puedePelear = puedePelear;
+            this.peleasGanadas = peleasGanadas;
         }
     } //Avenger
-    const antman = new Avenger();
+    const antman = new Avenger('Batman', 'Salon de la Justicia');
     console.log(antman);
     setTimeout(function () { location.reload(); }, 30000);
 })(); //FUNCION AUTOINVOCADA CON FLECHA
